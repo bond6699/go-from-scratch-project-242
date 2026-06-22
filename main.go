5ca4aed7adbbe4c6c5288324649358064c07f4dd
bigbo <lomalbtn693301@gmail.com>
2026-06-22 17:23:07 +0300

    Fix Bugs

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 
 // GetPathSize возвращает человекочитаемый размер пути в виде строки.
 // Рекурсивно обрабатывает директории, игнорирует скрытые файлы.
-func GetPathSize(path string) (string, error) {
+func GetPathSize(path string, recursive, human, all bool) (string, error) {
 	// Устанавливаем флаги: recursive=true, human=true, all=false
-	cliflags := flags.Create(true, true, false)
+	cliflags := flags.Create(recursive, all, human)
 
 	size, err := analyzer.Analyze(cliflags, path)
 	if err != nil {
