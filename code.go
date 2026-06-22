a5458318a57af923af7f1fbd1c80306243632833
bigbo <lomalbtn693301@gmail.com>
2026-06-22 12:31:41 +0300

    Fix Bugs

--- code.go
+++ code.go
@@ -1,1 +1,11 @@
 package code
+
+import (
+	"code/internal/analyzer"
+	"code/internal/flags"
+)
+
+func GetPathSize(path string) (int64, error) {
+	cliflags := flags.Create(true, true, true)
+	return analyzer.Analyzer()
+}
